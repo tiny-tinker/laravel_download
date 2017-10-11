a03421e1abc457e2597e711488466ac62d298fe0
MarkTinker <mwbdevelopment@gmail.com>
2017-10-10 22:46:55 -0600

    fix download error

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -38,25 +38,29 @@ class DownloadController extends Controller
         }
         */
         // Donwload file
-        $file_url = url('/').'/download/SetupOmniBazaar-Windows.exe';        
-        echo $file_url;
-        return;
+        //$file_url = url('/').'/download/SetupOmniBazaar-Windows.exe';        
+        $file_url = public_path().'/download/SetupOmniBazaar-Windows.exe';
+        //echo $file_url;
+        //return;
 
-        header('Content-Type: application/octet-stream');
-        header('Content-Transfer-Encoding: Binary'); 
+        //header('Content-Type: application/octet-stream');
+        //header('Content-Transfer-Encoding: Binary'); 
         $file_ext = '.exe';
 
         if ($request->session()->has('referrer_id'))
         {
             $referrerid = $request->session()->get('referrer_id', null);
-            header('Content-disposition: attachment; filename="SetupOmniBazaar-'.$referrerid.$file_ext.'"'); 
+            $filename = 'SetupOmniBazaar-'.$referrerid.$file_ext;
+            return response()->download($file_url, $filename);
+            //header('Content-disposition: attachment; filename="SetupOmniBazaar-'.$referrerid.$file_ext.'"'); 
         }
         else 
         {
-            header('Content-disposition: attachment; filename="SetupOmniBazaar'.$file_ext.'"'); 
+            //header('Content-disposition: attachment; filename="SetupOmniBazaar'.$file_ext.'"'); 
+            return response()->download($file_url);
         }
-        ob_clean(); flush();
-        readfile($file_url);
+        //ob_clean(); flush();
+        //readfile($file_url);
     }
 
     public function index(Request $request)
